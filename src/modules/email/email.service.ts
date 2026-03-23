31a56107e45429481ecd53711c5e0ae270a5730f
Jared-Are <142358454+Jared-Are@users.noreply.github.com>
2026-03-22 22:27:36 -0600

    probando

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -7,20 +7,11 @@ export class EmailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST || 'smtp.gmail.com',
-      port: parseInt(process.env.SMTP_PORT || '587', 10),
-      secure: process.env.SMTP_PORT === '465',
+      service: 'gmail',
       auth: {
       user: process.env.SMTP_USER || '',
         pass: process.env.SMTP_PASS || '',
       },
-      family: 4, 
-      tls: {
-        rejectUnauthorized: false
-      },
-      connectionTimeout: 10000, 
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
     }as any);
   }
 
