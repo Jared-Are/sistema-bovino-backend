ae76b98b3a6f00fd8c36e29913aeb3fed377008c
Jared-Are <142358454+Jared-Are@users.noreply.github.com>
2026-03-22 21:24:24 -0600

    imprimir errorv2

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -8,12 +8,16 @@ export class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST || 'smtp.gmail.com',
-      port: parseInt(process.env.SMTP_PORT || '587'),
-      secure: false,
+      port: parseInt(process.env.SMTP_PORT || '465'),
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      // 👇 El truco anti-cuelgues: Si en 10 segundos no responde, ¡cuelga y tira error!
+      connectionTimeout: 10000, 
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
     });
   }
 
